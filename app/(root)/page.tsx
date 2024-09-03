109f1fe5afaddacf48957f5e0ff88329348acfc8
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-09-03 21:23:56 +0530

    convex backend setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
@@ -17,6 +22,7 @@ const Home = () => {
             />
           ))}
         </div>
+        {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
       </section>
     </div>
   );
