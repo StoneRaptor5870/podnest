8e51a1318c3781170312ec12a2e5877bc1ddd821
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-09-07 02:25:18 +0530

    right side bar

---
+++ components/Carousel.tsx
@@ -0,0 +1,11 @@
+import { CarouselProps } from "@/types";
+
+const Carousel = ({ fansLikeDetails }: CarouselProps) => {
+  return (
+    <div>
+      Carousel
+    </div>
+  )
+}
+
+export default Carousel
--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,7 +1,50 @@
+"use client";
+
+import { SignedIn, useUser, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from 'next/link';
+import Carousel from "@/components/Carousel";
+import Header from "@/components/Header";
+import { useQuery } from "convex/react";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import { api } from "@/convex/_generated/api";
+import { useRouter } from "next/navigation";
+
 const RightSideBar = () => {
+  const router = useRouter();
+  const { user } = useUser();
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
+
+  if (!topPodcasters) {
+    return (
+      <div className="flex w-[310px] items-center justify-center h-full">
+        <LoaderSpinner />;
+      </div>
+    );
+  }
+
   return (
     <section className="right_sidebar text-white-1">
-      Right Side Bar
+      <SignedIn>
+        <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
+          <UserButton />
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-16 truncate font-semibold text-white-1">
+              {user?.firstName} {user?.lastName}
+            </h1>
+            <Image
+              src="/icons/right-arrow.svg"
+              alt="arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
+      </SignedIn>
+      <section className="flex flex-col gap-4">
+        <Header title="Fans Like You" />
+        <Carousel fansLikeDetails={topPodcasters!} />
+      </section>
     </section>
   )
 }
